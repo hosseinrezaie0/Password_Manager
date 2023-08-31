b94406840772ea281739e6ec637a4e2b2e5632d1
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2023-08-31 12:41:45 +0330

    Window added

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+#-----------------------UI design-----------------------#
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+
+
+
+window.mainloop()
