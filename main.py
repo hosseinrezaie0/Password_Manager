0ed57dd4afe06f6fa5a39500ef8009b03a8673cb
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2023-08-31 12:46:19 +0330

    logo added to main

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
+canavs = Canvas(width=200,height=200,highlightthickness=0,)
+photo = PhotoImage(file="logo.png")
+canavs.create_image(100,100,image=photo)
+canavs.grid(row=0,column=1)
+
 
 
 
