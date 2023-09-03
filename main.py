89c4e03af89a7ace9f0845e94f6f3cd1c42ee0c5
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2023-09-03 19:22:23 +0330

    The color of labels and buttons changed

--- main.py
+++ main.py
@@ -17,27 +17,27 @@ canavs.create_image(100,100,image=photo)
 canavs.grid(row=0,column=1)
 
 #Web
-web_label = Label(text="Website:",padx=20,pady=20)
+web_label = Label(text="Website:",padx=20,pady=20,bg=BACKGROUND_COLOR)
 web_label.grid(row=1,column=0)
-web_entry = Entry(width=35,)
+web_entry = Entry(width=36)
 web_entry.grid(row=1,column=1, columnspan=2)
 
 #Username
-email_label = Label(text="Email/Username:", padx=20,pady=20)
+email_label = Label(text="Email/Username:", padx=20,pady=20,bg=BACKGROUND_COLOR)
 email_label.grid(row=2, column=0)
-email_entry = Entry(width=35)
+email_entry = Entry(width=36)
 email_entry.grid(row=2,column=1,columnspan=2)
 
 #Password
-password_label = Label(text="Password", padx=20,pady=20)
+password_label = Label(text="Password", padx=20,pady=20,bg=BACKGROUND_COLOR)
 password_label.grid(row=3,column=0)
-password_entry = Entry(width=18)
+password_entry = Entry(width=19)
 password_entry.grid(row=3,column=1)
 
-password_btn = Button(text="Generate Password",bg="white",bd=1)
+password_btn = Button(text="Generate Password",bg=BUTTON_COLOR,bd=1)
 password_btn.grid(row=3,column=2)
 
-add_btn =Button(text="Add", bg="white", width=36,bd=1)
+add_btn =Button(text="Add", bg=BUTTON_COLOR, width=36,bd=1)
 add_btn.grid(row=4,column=1,columnspan=2)
 
 
